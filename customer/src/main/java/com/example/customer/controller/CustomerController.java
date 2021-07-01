a7eb5e8b4225717e82066efdf2884f15e3448b26
刘石次 <liushici@tuliu.com>
2021-07-01 11:44:09 +0800

    初始提交

---
+++ customer/src/main/java/com/example/customer/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.example.customer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/customer")
+    public String index(){
+        // 访问生产者 http://nacos-provide/index
+        return restTemplate.getForObject("http://nacos-provide/index",String.class);
+    }
+}
---
+++ producer/src/main/java/com/example/producer/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.producer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "生产数据：DEMO";
+    }
+}
